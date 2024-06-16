4de8afbbde88a16547fd8045ba198b7c24efecbc
yasin-arft <yasinarftsocial@gmail.com>
2024-06-16 23:31:38 +0600

    redirect when token not verified

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,10 +1,14 @@
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import useAuth from "./useAuth";
 
 const axiosSecure = axios.create({
   baseURL: import.meta.env.VITE_server_URL
 });
 
 const useAxiosSecure = () => {
+  const { logout, setLoading } = useAuth();
+  const navigate = useNavigate();
 
   // request interceptor
   axiosSecure.interceptors.request.use((config) => {
@@ -13,6 +17,23 @@ const useAxiosSecure = () => {
 
     return config
   }, (error) => {
+    return Promise.reject(error);
+  });
+
+  // response interceptor
+  axiosSecure.interceptors.response.use((response) => {
+    return response;
+  }, async (error) => {
+    const status = error.response.status;
+
+    if (status === 401) {
+      logout()
+        .then(() => {
+          setLoading(false);
+          navigate('/login');
+        })
+    }
+
     return Promise.reject(error);
   });
 
