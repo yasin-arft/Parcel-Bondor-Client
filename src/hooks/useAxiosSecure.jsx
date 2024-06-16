a0e2ff60aa2df65164507a5f9c57e4d5cc5f60da
yasin-arft <yasinarftsocial@gmail.com>
2024-06-16 22:51:20 +0600

    token send via interceptores requsest

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -7,6 +7,16 @@ const axiosSecure = axios.create({
 
 const useAxiosSecure = () => {
 
+  // request interceptor
+  axiosSecure.interceptors.request.use((config) => {
+    const token = localStorage.getItem('access-token');
+    config.headers.authorization = `Bearer ${token}`;
+
+    return config
+  }, (error) => {
+    return Promise.reject(error);
+  });
+
   return axiosSecure;
 };
 
