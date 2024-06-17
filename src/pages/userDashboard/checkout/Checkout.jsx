9e2e9abd7d0902b40594a09f75d0d7985f31cb5b
yasin-arft <yasinarftsocial@gmail.com>
2024-06-18 00:09:03 +0600

    price loaded in checkout page

--- src/pages/userDashboard/checkout/Checkout.jsx
+++ src/pages/userDashboard/checkout/Checkout.jsx
@@ -1,10 +1,25 @@
 import SectionHeading from "@/components/sectionHeading/SectionHeading";
+import useAxiosSecure from "@/hooks/useAxiosSecure";
+import { useQuery } from "@tanstack/react-query";
+import { useParams } from "react-router-dom";
 
 const Checkout = () => {
+  const axiosSecure = useAxiosSecure();
+  const { id } = useParams();
+  const { data, isLoading } = useQuery({
+    queryKey: ['booking checkout', id],
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/booking/${id}`);
+      return res.data;
+    }
+  });
+
+  if (isLoading) return
+
   return (
     <section>
       <SectionHeading>Checkout</SectionHeading>
-      
+      <h2 className="text-2xl font-medium">Your bill is : {data.price}tk</h2>
     </section>
   );
 };
